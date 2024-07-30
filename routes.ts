05f3668cc06b18b189e728fcc939695ea086f9c4
jieun <forworkje21@gmail.com>
2024-07-30 22:06:47 +0900

    Feat: 타입, 스크린 관련 상수 추가

---
+++ routes.ts
@@ -0,0 +1,12 @@
+export const RouteNames = {
+  HOME: 'home' as const,
+  HOME_TAB: 'home-tab' as const,
+  SHOPPING: 'shopping' as const,
+  BROWSER: 'browser' as const,
+}
+// as const: unique 한 타입을 의미함
+
+export type RootStackParamList = {
+  [RouteNames.HOME_TAB]: undefined
+  [RouteNames.BROWSER]: undefined
+}
