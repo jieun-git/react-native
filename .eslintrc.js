2dca570a50fea60a9d57b4f8cd1cb9d99942fc83
jieun <forworkje21@gmail.com>
2024-07-30 19:07:27 +0900

    Feat: 프리티어 설정 추가(세미콜론x, 띄어쓰기)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+  // root: true,
+  // extends: '@react-native',
+  parser: '@babel/eslint-parser', // Babel ESLint 파서 사용
+  rules: {
+    quotes: ['error', 'single'],
+    // 다른 규칙들...
+  },
+  settings: {
+    react: {
+      version: 'detect', // React 버전을 자동으로 감지하여 사용
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 2021, // 또는 최신 ES 버전 숫자
+    sourceType: 'module', // ES6 모듈을 사용하기 위해 설정
+    requireConfigFile: false, // Babel 설정 파일이 없어도 되도록 설정
+    // babelOptions: {
+    //   presets: ['@babel/preset-env', '@babel/preset-react'], // 필요한 경우 프리셋 추가
+    // },
+  },
+}
