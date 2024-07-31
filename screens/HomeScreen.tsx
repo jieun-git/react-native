7c587180a41893fa7b0a126d6b32c8bbbe58f93e
jieun <forworkje21@gmail.com>
2024-07-31 12:44:41 +0900

    Feat: 네이버 모바일 웹 띄우기

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,21 @@
-import { Text } from 'react-native'
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import WebView from 'react-native-webview'
+
+const styles = StyleSheet.create({
+    safearea: { flex: 1 },
+})
 
 const HomeScreen = () => {
-  return <Text>Home</Text>
+    return (
+        // SafeAreaView: 노치 디자인을 고려하여 안전 영역 경계 안에서 컨텐츠를 렌더링하는 것. IOS11 이후 아이폰에 적용하면 노치에 웹뷰 표시 안 됨
+        <SafeAreaView style={styles.safearea}>
+            <WebView
+                source={{ uri: 'https://m.naver.com/' }}
+                showsVerticalScrollIndicator={false}
+                showHorizontalScrollIndicator={false}
+            />
+        </SafeAreaView>
+    )
 }
 
 export default HomeScreen
