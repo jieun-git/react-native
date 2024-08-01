54c57812f39774e3d417eb2c95880a124414f526
jieun <forworkje21@gmail.com>
2024-08-02 08:40:45 +0900

    Refactor: 쇼핑 스크린 네이버 쇼핑 사이트로 네비게이션 처리

--- screens/ShoppingScreen.tsx
+++ screens/ShoppingScreen.tsx
@@ -1,28 +1,48 @@
-import { Text, TouchableOpacity, View } from 'react-native'
-import { RootStackParamList, RouteNames } from '../routes.ts'
+import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import WebView from 'react-native-webview'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { RootStackParamList, RouteNames } from '../routes.ts'
 
 type Props = NativeStackScreenProps<RootStackParamList>
 
-// navigationContainer 로 감싸져있는 컴포넌트는 navigation props 를 전달받지 않아도 갖게 됨.
-// onPress 라는 속성으로 특정 스크린으로 이동할 수 있음
+const styles = StyleSheet.create({
+    safearea: { flex: 1 },
+})
+
+const SHOPPING_HOME_URL = 'https://shopping.naver.com/home'
+
+// request 가 홈 화면이랑 관련 된 것이면 true를 리턴, 그 외에는 다른 스크린으로 보여줌
 const ShoppingScreen = ({ navigation }: Props) => {
     return (
-        <View>
-            <Text>Shopping</Text>
-            <TouchableOpacity
-                onPress={() => {
-                    navigation.navigate(RouteNames.BROWSER, {
-                        // 임시 url
-                        initialUrl: 'https://m.naver.com',
-                    })
+        // SafeAreaView: 노치 디자인을 고려하여 안전 영역 경계 안에서 컨텐츠를 렌더링하는 것. IOS11 이후 아이폰에 적용하면 노치에 웹뷰 표시 안 됨
+        <SafeAreaView style={styles.safearea}>
+            <WebView
+                source={{ uri: SHOPPING_HOME_URL }}
+                showsVerticalScrollIndicator={false}
+                showHorizontalScrollIndicator={false}
+                onShouldStartLoadWithRequest={(request) => {
+                    if (
+                        request.url.startsWith(SHOPPING_HOME_URL) ||
+                        request.mainDocumentURL?.startsWith(SHOPPING_HOME_URL)
+                    ) {
+                        // url 이 m.naver.com 으로 시작하면 홈스크린에서 보여준다.
+                        return true
+                    }
+
+                    if (
+                        request.url != null &&
+                        request.url.startsWith('https://')
+                    ) {
+                        // url 이 m.naver.com 으로 시작하지 않으면 브라우저 스크린으로 라우팅시키면서 요청받은 url로 보내겠다.
+                        navigation.navigate(RouteNames.BROWSER, {
+                            initialUrl: request.url,
+                        })
+                        return false
+                    }
+                    return true
                 }}
-            >
-                <Text>Go To Browser</Text>
-            </TouchableOpacity>
-            <MaterialCommunityIcons name="shopping" size={50} color="pink" />
-        </View>
+            />
+        </SafeAreaView>
     )
 }
 
