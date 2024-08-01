dd2ba18afe6ce2e00ceabb1803565bbd5dca58b9
jieun <forworkje21@gmail.com>
2024-08-01 09:18:03 +0900

    Refactor: url 간소화

--- screens/BrowserScreen.tsx
+++ screens/BrowserScreen.tsx
@@ -2,6 +2,7 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import WebView from 'react-native-webview'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../routes.ts'
+import { useMemo, useState } from 'react'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'browser'>
 
@@ -18,12 +19,25 @@ const style = StyleSheet.create({
 
 const ShoppingScreen = ({ route }: Props) => {
     const { initialUrl } = route.params
+
+    const [url, setUrl] = useState<string>(initialUrl)
+    // url 이 바뀔 때만 연산하도록 하여 렌더링 최적화
+    const urlTitle = useMemo(
+        () => url.replace('https://', '').split('/')[0],
+        [url],
+    )
+
     return (
         <SafeAreaView style={style.safearea}>
             <View style={style.urlContainer}>
-                <Text style={style.urlText}>URL</Text>
+                <Text style={style.urlText}>{urlTitle}</Text>
             </View>
-            <WebView source={{ uri: initialUrl }} />
+            <WebView
+                source={{ uri: initialUrl }}
+                onNavigationStateChange={(event) => {
+                    setUrl(event.url)
+                }}
+            />
         </SafeAreaView>
     )
 }
