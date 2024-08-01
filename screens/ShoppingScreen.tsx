1ecffec14accf1db8c5288af5cc16e380bd86a98
jieun <forworkje21@gmail.com>
2024-08-01 20:34:56 +0900

    Wip: 쇼핑 스크린 테스트 구현

--- screens/ShoppingScreen.tsx
+++ screens/ShoppingScreen.tsx
@@ -8,18 +8,22 @@ type Props = NativeStackScreenProps<RootStackParamList>
 // navigationContainer 로 감싸져있는 컴포넌트는 navigation props 를 전달받지 않아도 갖게 됨.
 // onPress 라는 속성으로 특정 스크린으로 이동할 수 있음
 const ShoppingScreen = ({ navigation }: Props) => {
-  return (
-    <View>
-      <Text>Shopping</Text>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate(RouteNames.BROWSER)
-        }}>
-        <Text>Go To Browser</Text>
-      </TouchableOpacity>
-      <MaterialCommunityIcons name="shopping" size={50} color="pink" />
-    </View>
-  )
+    return (
+        <View>
+            <Text>Shopping</Text>
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.navigate(RouteNames.BROWSER, {
+                        // 임시 url
+                        initialUrl: 'https://m.naver.com',
+                    })
+                }}
+            >
+                <Text>Go To Browser</Text>
+            </TouchableOpacity>
+            <MaterialCommunityIcons name="shopping" size={50} color="pink" />
+        </View>
+    )
 }
 
 export default ShoppingScreen
