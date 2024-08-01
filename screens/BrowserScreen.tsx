8b82971334f4efc76a69b85fb39042d1ed00b6e5
jieun <forworkje21@gmail.com>
2024-08-01 09:12:22 +0900

    Styles: 브라우저 웹뷰 헤더 스타일링

--- screens/BrowserScreen.tsx
+++ screens/BrowserScreen.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import WebView from 'react-native-webview'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../routes.ts'
@@ -6,13 +6,23 @@ import { RootStackParamList } from '../routes.ts'
 type Props = NativeStackScreenProps<RootStackParamList, 'browser'>
 
 const style = StyleSheet.create({
-    safearea: { flex: 1 },
+    safearea: { flex: 1, backgroundColor: 'black' },
+    urlContainer: {
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 5,
+    },
+    urlText: { color: 'white' },
 })
 
 const ShoppingScreen = ({ route }: Props) => {
     const { initialUrl } = route.params
     return (
         <SafeAreaView style={style.safearea}>
+            <View style={style.urlContainer}>
+                <Text style={style.urlText}>URL</Text>
+            </View>
             <WebView source={{ uri: initialUrl }} />
         </SafeAreaView>
     )
