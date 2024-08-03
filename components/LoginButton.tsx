6fe33563c82d99aec2ead03cf688208569fa0f82
jieun <forworkje21@gmail.com>
2024-08-03 15:45:19 +0900

    Feat: 쿠키로 로그인 상태를 받아오고 상태에 따라 로그인/아웃 버튼 분기 처리

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,22 +1,37 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import { useNavigation } from '@react-navigation/native'
+import { useIsFocused, useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParamList, RouteNames } from '../routes.ts'
+import CookieManager from '@react-native-cookies/cookies'
 
 type Props = NativeStackNavigationProp<RootStackParamList>
 
 const LoginButton = () => {
     const navigation = useNavigation<Props>()
 
-    const isLoggedIn = false
+    // Home/Shopping 스크린인 경우 true/false, LoginButton 이 Home/Shopping 에만 존재
+    const isFocused = useIsFocused()
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
     const iconName = isLoggedIn ? 'logout' : 'login'
 
     const onPressLogin = useCallback(async () => {
         navigation.navigate(RouteNames.LOGIN)
     }, [navigation])
     const onPressLogout = useCallback(async () => {}, [])
+
+    useEffect(() => {
+        if (isFocused) {
+            CookieManager.get('https://www.naver.com', true).then((cookie) => {
+                if (cookie.NID_SES) {
+                    setIsLoggedIn(true)
+                } else {
+                    setIsLoggedIn(false)
+                }
+            })
+        }
+    }, [isFocused])
 
     return (
         <TouchableOpacity onPress={isLoggedIn ? onPressLogout : onPressLogin}>
