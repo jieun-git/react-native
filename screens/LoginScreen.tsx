b514c93e88d73af25e72512782eff59800fd4b9e
jieun <forworkje21@gmail.com>
2024-08-03 12:59:31 +0900

    Feat: props가 아닌 Hook 을 이용하여 내비게이션 정의

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 import WebView from 'react-native-webview'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../routes.ts'
+import { useNavigation } from '@react-navigation/native'
+
+type Props = NativeStackScreenProps<RootStackParamList>
 
 const styles = StyleSheet.create({
     safearea: { flex: 1, backgroundColor: 'black' },
@@ -9,6 +14,8 @@ const styles = StyleSheet.create({
 const LOGIN_URL = 'https://nid.naver.com/nidlogin.login'
 
 const LoginScreen = () => {
+    const navigation = useNavigation<Props>()
+
     return (
         <SafeAreaView style={styles.safearea}>
             <WebView source={{ uri: LOGIN_URL }} />
